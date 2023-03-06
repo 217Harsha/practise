e98ddf886027dcfedd7bffe588219a386ba1bbdc
leela krishna <leelakrishnaboddapati6420@gmail.com>
2023-03-06 09:30:55 +0530

    adding discovery ability to register themselves with service-registry

--- order-service/order-service/src/main/java/com/leela/orderservice/OrderServiceApplication.java
+++ order-service/order-service/src/main/java/com/leela/orderservice/OrderServiceApplication.java
@@ -4,10 +4,12 @@ import com.leela.orderservice.dto.Payment;
 import com.leela.orderservice.dto.TransactionResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
